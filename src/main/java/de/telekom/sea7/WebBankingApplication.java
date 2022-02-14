fd945296f7f7e0dccf037ac61f9910609bd26c38
Oliver Reiger <oliver.reiger@mac.com>
2022-02-14 12:58:47 +0100

    Changes in the gitignore

--- src/main/java/de/telekom/sea7/WebBankingApplication.java
+++ src/main/java/de/telekom/sea7/WebBankingApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebBankingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebBankingApplication.class, args);
 	}
